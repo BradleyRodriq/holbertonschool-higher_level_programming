925c7766c0cad6521e77fc718d9ad563a59631a5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-29 22:19:55 -0400

    task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    
+    idx = list(a_dictionary.keys())[0]
+    best = a_dictionary[idx]
+    for key, value in a_dictionary.items():
+        if value > best:
+            best = value
+            idx = key
+    return idx
