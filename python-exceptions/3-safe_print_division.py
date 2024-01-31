d96023d5aa37e42d4194966a1d0be0bea161f94f
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-31 09:58:46 -0400

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (TypeError, ZeroDivisionError):
+    except TypeError:
+        result = None
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside Result: {}".format(result))
