ef2ecd6ad1509e526430435156dea998f3a00fc5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-02-02 18:03:08 -0400

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,10 +8,10 @@ def print_square(size):
     Args:
         size: size of the square
     """
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         for j in range(size):
