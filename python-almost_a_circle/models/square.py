6a777c9f3314ae75e1a6d91a078a2d2346e1dcdd
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-02-13 08:59:05 -0400

    task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square Class
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialize a Square instance.
