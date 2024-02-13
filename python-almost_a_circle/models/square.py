4ecff413152e506c229ecbd1ef91cccbee98b5e1
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-02-13 08:58:28 -0400

    task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,17 +5,50 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square's position.
+            y (int): The y-coordinate of the square's position.
+            id (int): The ID of the square.
+
+        Returns:
+            None
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Returns:
+            None
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """
+        Return a string representation of the square.
 
+        Returns:
+            str: The string representation of the square.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y, self.width)
