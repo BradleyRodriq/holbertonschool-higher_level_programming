202addb402454b7c52027e4bda0e2cf06b3f68f1
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-02-12 22:38:41 -0400

    task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initializes a Rectangle object.
