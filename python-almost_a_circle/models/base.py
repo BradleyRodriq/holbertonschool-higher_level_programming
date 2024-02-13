6f9a7115a3bff16b015dcc8ff80d3294e54900bc
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-02-13 09:25:31 -0400

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,6 +7,7 @@ class Base():
     """
     Base class for all other classes in the project.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -22,3 +23,18 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: A JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
