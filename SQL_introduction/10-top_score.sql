50d0ed3110207b27ea58e91d96db5210074b740d
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-15 18:25:11 -0400

    list by best

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
---lists all records of second_table in order of scores --
-
+-- lists all records of second_table in order of scores --
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
