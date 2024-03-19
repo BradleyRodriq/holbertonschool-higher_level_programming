f4bec0f125436b1c2e65b016a248f7c40b4581c7
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-18 22:37:56 -0400

    task 0 pycode fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-
+""" list all states"""
 import MySQLdb
 import sys
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    """ list all states """
     dtb = MySQLdb.connect(user=sys.arg[1], passwd=sys.arg[2], db=sys.arg[3])
     cursor = dtb.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
