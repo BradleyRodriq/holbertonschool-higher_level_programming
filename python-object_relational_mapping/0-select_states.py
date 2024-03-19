2896d8789e7d6eed73ef6b470ff761f9dd554dbe
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-18 22:54:51 -0400

    fix for task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" list all states"""
+""" list all states from the database """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    """ list all states """
+    """ list all states from the database """
     dtb = MySQLdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
     cursor = dtb.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
