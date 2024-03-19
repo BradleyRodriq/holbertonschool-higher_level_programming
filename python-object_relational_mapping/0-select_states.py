6a8ca9e282bee7fd1cfec77d52c85a82762c9efe
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-18 23:13:08 -0400

    fixed filter task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     """ list all states from the database """
     """
     password = '2792'
-    dtb = MySQLdb.connect(host= 'localhost', user='root', port=3306, passwd=password, db='hbtn_0e_0_usa')
+    dtb = MySQLdb.connect(host= 'localhost', user='root', port=3306,
+    passwd=password, db='hbtn_0e_0_usa')
     """
     dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                           passwd=sys.argv[2], db=sys.argv[3])
