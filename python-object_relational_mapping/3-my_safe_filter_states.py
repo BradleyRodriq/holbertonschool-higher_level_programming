22f2988f3483393fae36640e0a8405df9a340fe5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-20 09:43:10 -0400

    task 3 was running local code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,19 +6,18 @@ import sys
 
 if __name__ == "__main__":
     """ displays values in states where name matched the argument 4 """
+    """
     password = '2792'
     dtb = MySQLdb.connect(host= 'localhost', user='root', port=3306,
     passwd=password, db='hbtn_0e_0_usa')
     """
     dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                           passwd=sys.argv[2], db=sys.argv[3])
-                          """
     curs = dtb.cursor()
     curs.execute("SELECT * FROM states "
                 "WHERE name LIKE BINARY %(name)s "
                 "ORDER BY states.id ASC ",
                 {'name': sys.argv[4]})
     rows = curs.fetchall()
-    if rows is not None:
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
