448701abb38391f65a8ca6155a7aee5e5a9c3ce4
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-20 10:11:55 -0400

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
     dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                           passwd=sys.argv[2], db=sys.argv[3])
     curs = dtb.cursor()
-    curs.execute("SELECT * FROM states "
-                "WHERE name LIKE BINARY %(name)s "
-                "ORDER BY states.id ASC ",
-                {'name': sys.argv[4]})
+    function =  "SELECT * FROM states \
+                WHERE name LIKE BINARY %(name)s \
+                ORDER BY states.id ASC "
+    curs.execute(function, (sys.argv[4]))
     rows = curs.fetchall()
     for row in rows:
         print(row)
