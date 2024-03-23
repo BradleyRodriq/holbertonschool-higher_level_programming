0941a95f3f4f4c4f7157b1606a93b8f5be19dd96
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-03-23 11:46:09 -0400

    added fibonacci practice

---
+++ python-practice/fibonacci.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def fibonacci(num):
+    sequence = [10, 20]
+    for i in range(2, n):
+        sequence.append(sequence[i-1] + sequence[i-2])
+    return sequence
+
+
+for n in range(1, 11):
+    print(f"Fibonacci sequence with {n} terms:", fibonacci(n))
